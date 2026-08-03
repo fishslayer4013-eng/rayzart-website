1370db5f2a0c3d3bb5222d8243d7e429f804842a
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-03 15:58:49 -0700

    Connect Rayzart business email

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ window.RAYZART_CONFIG = {
   businessName: "Rayzart Diversified Services LLC",
   shortName: "Rayzart",
   publicPhone: "208-691-2496",
-  publicEmail: "",
+  publicEmail: "bill@rayzartllc.com",
   websiteUrl: "https://fishslayer4013-eng.github.io/rayzart-website/",
   pickupLocation: "Hauser, Idaho",
   serviceArea: "North Idaho & the Spokane Area"
