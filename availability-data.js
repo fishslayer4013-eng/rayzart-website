919ee3645b9cd7fbffa6c06f4665bbaa1a2c950d
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-04 08:35:51 -0700

    Add trailer availability data

---
+++ availability-data.js
@@ -0,0 +1,23 @@
+window.RAYZART_AVAILABILITY = {
+  updated: "2026-08-04",
+  bookings: [
+    {
+      trailer: "23 Deck Trailer",
+      start: "2026-08-05",
+      end: "2026-08-05",
+      status: "booked"
+    },
+    {
+      trailer: "Deck Trailers",
+      start: "2026-08-06",
+      end: "2026-08-06",
+      status: "limited"
+    },
+    {
+      trailer: "Deck Trailers",
+      start: "2026-08-21",
+      end: "2026-08-23",
+      status: "limited"
+    }
+  ]
+};
