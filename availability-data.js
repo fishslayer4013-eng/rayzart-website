211dfc082e4d64d7b245585ba489622b6000411b
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-04 13:58:39 -0700

    Add Corey 26 Deck booking for Aug 5

--- availability-data.js
+++ availability-data.js
@@ -9,6 +9,12 @@ window.RAYZART_AVAILABILITY = {
     },
     {
       trailer: "26 Deck Trailer",
+      start: "2026-08-05",
+      end: "2026-08-05",
+      status: "booked"
+    },
+    {
+      trailer: "26 Deck Trailer",
       start: "2026-08-06",
       end: "2026-08-06",
       status: "booked"
