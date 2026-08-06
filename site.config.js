3ae75db4a3cb87b29276b5da9d028cbd991f627e
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-06 09:32:48 -0700

    Use custom domain in live site configuration

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ window.RAYZART_CONFIG = {
   shortName: "Rayzart",
   publicPhone: "208-691-2496",
   publicEmail: "bill@rayzartllc.com",
-  websiteUrl: "https://fishslayer4013-eng.github.io/rayzart-website/",
+  websiteUrl: "https://rayzartllc.com",
   pickupLocation: "Hauser, Idaho",
   serviceArea: "North Idaho & the Spokane Area"
 };
