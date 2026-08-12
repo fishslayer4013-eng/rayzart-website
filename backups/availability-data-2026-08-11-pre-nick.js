995d1130eab7afc5ff8d8c044b5e7109b5c5b78e
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-11 21:43:14 -0700

    Backup availability data before Nick Nelson rental update

---
+++ backups/availability-data-2026-08-11-pre-nick.js
@@ -0,0 +1,23 @@
+window.RAYZART_AVAILABILITY = {
+  updated: "2026-08-11",
+  bookings: [
+    {
+      trailer: "26 Dump Trailer",
+      start: "2026-08-10",
+      end: "2026-08-13",
+      status: "booked"
+    },
+    {
+      trailer: "23 Deck Trailer",
+      start: "2026-08-21",
+      end: "2026-08-23",
+      status: "booked"
+    },
+    {
+      trailer: "26 Deck Trailer",
+      start: "2026-08-21",
+      end: "2026-08-24",
+      status: "booked"
+    }
+  ]
+};
