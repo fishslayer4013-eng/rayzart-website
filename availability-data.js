8d449f8417e48c4a7526201c92a9f05bdd2c98ba
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-14 07:32:10 -0700

    Sync trailer availability from RBMS

--- availability-data.js
+++ availability-data.js
@@ -2,12 +2,6 @@ window.RAYZART_AVAILABILITY = {
   updated: "2026-08-14",
   bookings: [
     {
-      trailer: "26 Dump Trailer",
-      start: "2026-08-13",
-      end: "2026-08-14",
-      status: "booked"
-    },
-    {
       trailer: "23 Deck Trailer",
       start: "2026-08-21",
       end: "2026-08-23",
