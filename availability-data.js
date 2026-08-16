95d8e54e04366ff8e505ed4523b33bbc6eb05066
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-16 12:51:15 -0700

    Make 26 Dump Trailer available after return

--- availability-data.js
+++ availability-data.js
@@ -2,12 +2,6 @@ window.RAYZART_AVAILABILITY = {
   updated: "2026-08-16",
   bookings: [
     {
-      trailer: "26 Dump Trailer",
-      start: "2026-08-15",
-      end: "2026-08-16",
-      status: "booked"
-    },
-    {
       trailer: "23 Deck Trailer",
       start: "2026-08-21",
       end: "2026-08-23",
