fbf3476ef5e24eac273b64a11033358ab2e1c44d
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-24 21:31:27 -0700

    Remove stale dump trailer hold

--- availability-data.js
+++ availability-data.js
@@ -1,12 +1,4 @@
 window.RAYZART_AVAILABILITY = {
   updated: "2026-08-24",
-  unavailableTrailers: ["26 Dump Trailer"],
-  bookings: [
-    {
-      trailer: "26 Dump Trailer",
-      start: "2026-08-24",
-      end: "2026-09-21",
-      status: "booked"
-    }
-  ]
+  bookings: []
 };
