c490b2cf2e0514f08428eed2fa51361253c960fe
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-24 20:50:40 -0700

    Match dump trailer block to Sept 21 review plan

--- availability-data.js
+++ availability-data.js
@@ -1,10 +1,11 @@
 window.RAYZART_AVAILABILITY = {
   updated: "2026-08-24",
+  unavailableTrailers: ["26 Dump Trailer"],
   bookings: [
     {
       trailer: "26 Dump Trailer",
       start: "2026-08-24",
-      end: "2027-08-31",
+      end: "2026-09-21",
       status: "booked"
     }
   ]
