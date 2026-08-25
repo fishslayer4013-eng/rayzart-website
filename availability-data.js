90803ba23f90e6fdcb3edfefa1549a8e15c64a47
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-24 20:45:42 -0700

    Mark dump trailer unavailable after accident

--- availability-data.js
+++ availability-data.js
@@ -1,4 +1,11 @@
 window.RAYZART_AVAILABILITY = {
   updated: "2026-08-24",
-  bookings: []
+  bookings: [
+    {
+      trailer: "26 Dump Trailer",
+      start: "2026-08-24",
+      end: "2027-08-31",
+      status: "booked"
+    }
+  ]
 };
