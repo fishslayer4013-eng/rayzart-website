41b86e4a6bcb13dc034cb5965d7e5539904b29c2
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-24 18:36:31 -0700

    Mark deck trailers available

--- availability-data.js
+++ availability-data.js
@@ -1,6 +1,4 @@
 window.RAYZART_AVAILABILITY = {
   updated: "2026-08-24",
-  bookings: [
-    { trailer: "26 Deck Trailer", start: "2026-08-21", end: "2026-08-24", status: "booked" }
-  ]
+  bookings: []
 };
