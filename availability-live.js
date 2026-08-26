67bb1179cb9b18a4f7e87d4c4522dbac0a9ace5c
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-26 16:39:46 -0700

    Restore dump trailer booked dates

--- availability-live.js
+++ availability-live.js
@@ -1,4 +1,11 @@
 window.RAYZART_AVAILABILITY = {
   updated: "2026-08-26",
-  bookings: []
+  bookings: [
+    {
+      trailer: "26 Dump Trailer",
+      start: "2026-08-22",
+      end: "2026-09-20",
+      status: "booked"
+    }
+  ]
 };
