a0b3f5d80b4a93d4fdee8babaf9b5b38556fdbf4
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-08-31 13:47:49 -0700

    Block Trailer 1 for tentative Sept 3-4 shed job

--- availability-data.js
+++ availability-data.js
@@ -1,7 +1,8 @@
 window.RAYZART_AVAILABILITY = {
-  updated: "2026-08-30",
+  updated: "2026-08-31",
   bookings: [
-    { trailer: "26 Dump Trailer", start: "2026-08-22", end: "2026-09-21", status: "booked" }
+    { trailer: "26 Dump Trailer", start: "2026-08-22", end: "2026-09-21", status: "booked" },
+    { trailer: "23 Deck Trailer", start: "2026-09-03", end: "2026-09-04", status: "booked" }
   ]
 };
 
