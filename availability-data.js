e624490993036399226bfbd2b0a12a1efdfb5489
fishslayer4013-eng <fishslayer4013@gmail.com>
2026-09-25 08:37:04 -0700

    Mark Owen 23 deck rental completed

--- availability-data.js
+++ availability-data.js
@@ -1,6 +1,6 @@
 (function () {
   const data = {
-    updated: "2026-09-24",
+    updated: "2026-09-25",
     bookings: [
       { trailer: "23 Deck Trailer", start: "2026-09-04", end: "2026-09-04", status: "completed" },
       { trailer: "26 Dump Trailer", start: "2026-09-04", end: "2026-09-05", status: "booked" },
@@ -11,7 +11,7 @@
       { trailer: "26 Dump Trailer", start: "2026-09-14", end: "2026-09-20", status: "completed" },
       { trailer: "23 Deck Trailer", start: "2026-09-17", end: "2026-09-18", status: "completed" },
       { trailer: "26 Dump Trailer", start: "2026-09-21", end: "2026-09-21", status: "completed" },
-      { trailer: "23 Deck Trailer", start: "2026-09-21", end: "2026-09-22", status: "booked" },
+      { trailer: "23 Deck Trailer", start: "2026-09-21", end: "2026-09-22", status: "completed" },
       { trailer: "23 Deck Trailer", start: "2026-09-23", end: "2026-09-24", status: "completed" },
       { trailer: "23 Deck Trailer", start: "2026-09-26", end: "2026-09-27", status: "booked" }
     ]
